103632ea44eee59cc39cc5e0dbf1c01a30bb3de8
AxelDB96 <baldovinoaxel757@gmail.com>
2024-11-13 19:28:45 -0300

    13/11 editarUsuario falta terminar.

--- SRVCAplicacion/wwwroot/jsc/usuario.js
+++ SRVCAplicacion/wwwroot/jsc/usuario.js
@@ -34,9 +34,56 @@ async function mostrarUsuarios() {
     }
 }
 
+//cargaDatosParaEditar
+async function mostrarParaEditar() {
+    const response = await fetch('https://localhost:7285/api/Usuario/Obtener');
+    const users = await response.json();
+    const tableBody = document.getElementById('usuarioTableEdit').querySelector('tbody');
+    tableBody.innerHTML = '';
+    users.forEach(usuario => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+                <td>${usuario.id}</td>
+                <td><input type="text" value="${usuario.usuario}" data-id="${usuario.id}" data-field="usuario"/>
+                <td><input type="text" value="${usuario.contraseña}" data-id="${usuario.id}" data-field="contraseña"/>
+                <td><input type="text" value="${usuario.email}" data-id="${usuario.id}" data-field="email"/>
+                <td><input type="text" value="${usuario.telefono}" data-id="${usuario.id}" data-field="telefono"/>
+                <td><input type="text" value="${usuario.dni}" data-id="${usuario.id}" data-field="dni"/>
+                <td><input type="text" value="${usuario.estado}" data-id="${usuario.id}" data-field="estado"/>
+                <td><button onclick="editUsuario(${usuario.id})">Guardar Cambios</button></td>
+            `;
+        tableBody.appendChild(row);
+    });
+}
+
+//aceptarCambios
+async function editUsuario(id) {
+    const row = document.querySelector(`tr^[data-id="${id}"]`);
+    const usuario = row.querySelector('input[data-field="usuario"]').value;
+    const contraseña = row.querySelector('input[data-field="contraseña"]').value;
+    const email = row.querySelector('input[data-field="email"]').value;
+    const telefono = row.querySelector('input[data-field="telefono"]').value;
+    const dni = row.querySelector('input[data-field="dni"]').value;
+    const estado = row.querySelector('input[data-field="estado"]').value;
+
+    const response = await fetch(`https://localhost:7285/api/usuario/Actualizar/${id}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id, usuario, contraseña, email, telefono, dni, estado })
+    });
+    if (response.ok) {
+        alert("El usuario se actualizo correctamente.");
+        //fetchUsers();//Sepuedesacar
+    } else {
+        alert("Error al intentar aplicar los cambios.");
+    }
+} 
+
 
 //asdasd
-    
+
 
 
 //Funcion para cargar los datos del usuario elegido(admin)
@@ -61,9 +108,9 @@ async function cargarDatosUsuario(id_usuario) {
         console.error('Error al intentar cargar los datos del usuario', error);
     }
 }
-
+*/
 //Funcion para guardar los cambios del usuario elegido(admin)
-
+/*
 async function guardarCambiosUsuario(id_usuario) {
     const id_usuario = document.getElementById('id_usuario').value;
     const usuarioEditado = {
@@ -95,6 +142,8 @@ async function guardarCambiosUsuario(id_usuario) {
         console.error('Error al guardar cambios del usuario:', error);
     }
 }
+
+
 
 //Funcion para crear un nuevo usuario(admin)
 
