6356318074a9233c16d50f2cc648d17be680d4d5
AxelDB96 <baldovinoaxel757@gmail.com>
2024-11-15 21:02:28 -0300

    15/11 Ya funciona el editar usuario.

--- SRVCAplicacion/wwwroot/jsc/usuario.js
+++ SRVCAplicacion/wwwroot/jsc/usuario.js
@@ -13,7 +13,6 @@ async function mostrarUsuarios() {
         console.log('Usuarios recibidos:', usuarios);
 
         const tabla = document.getElementById('usuarioTable');
-       // tabla.innerHTML = ''; Limpia la tabla
 
         usuarios.forEach(usuario => {
             const fila = document.createElement('tr');
@@ -57,41 +56,6 @@ async function mostrarParaEditar() {
     });
 }
 
-//aceptarCambios
-/*
-
-async function editUsuario(id) {
-    const row = document.querySelector(`tr[data-id="${id}"]`);
-    console.log(row); // Verifica si row es null
-    if (!row) {
-        alert(`No se encontró la fila con el id: ${id}`);
-        return;
-    }
-    const usuario = row.querySelector('input[data-field="usuario"]').value;
-    const contraseña = row.querySelector('input[data-field="contraseña"]').value;
-    const email = row.querySelector('input[data-field="email"]').value;
-    const telefono = row.querySelector('input[data-field="telefono"]').value;
-    const dni = row.querySelector('input[data-field="dni"]').value;
-    const estado = row.querySelector('input[data-field="estado"]').value;
-
-    const response = await fetch(`https://localhost:7285/api/usuario/Actualizar/${id}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ usuario, contraseña, email, telefono, dni, estado })
-    });
-    if (response.ok) {
-        alert("El usuario se actualizo correctamente.");
-        //fetchUsers();//Sepuedesacar
-    } else {
-        alert("Error al intentar aplicar los cambios.");
-    }
-} 
-*/
-
-//asdasd
-
 async function editUsuario(id) {
     // Obtiene todos los inputs del usuario específico basándose en el `id`
     const inputs = document.querySelectorAll(`input[data-id="${id}"]`);
@@ -123,66 +87,6 @@ async function editUsuario(id) {
         console.error('Error al editar el usuario:', error);
     }
 }
-
-
-//Funcion para cargar los datos del usuario elegido(admin)
-
-/*
-async function cargarDatosUsuario(id_usuario) {
-    try {
-        const response = await fetch('api');
-        if (!response.ok) throw new Error('Error al intentar cargar los datos del usuario');
-
-        const usuario = await response.json();
-
-        document.getElementById('id_usuario').value = usuario.id_usuario;
-        document.getElementById('usuario').value = usuario.usuario;
-        document.getElementById('contraseña').value = usuario.contraseña;
-        document.getElementById('email').value = usuario.email;
-        document.getElementById('telefono').value = usuario.telefono;
-        document.getElementById('dni').value = usuario.dni;
-        document.getElementById('estado').value = usuario.estado;
-        document.getElementById('id_punto_control').value = usuario.id_punto_control;
-    } catch (error) {
-        console.error('Error al intentar cargar los datos del usuario', error);
-    }
-}
-*/
-//Funcion para guardar los cambios del usuario elegido(admin)
-/*
-async function guardarCambiosUsuario(id_usuario) {
-    const id_usuario = document.getElementById('id_usuario').value;
-    const usuarioEditado = {
-        Nombre: document.getElementById('usuario').value,
-        Contraseña: document.getElementById('contraseña').value;
-        EMAIL: document.getElementById('email').value;
-        Telefono: document.getElementById('telefono').value;
-        DNI: document.getElementById('dni').value;
-        Estado: document.getElementById('estado').value;
-        IDpuntoDeControl: document.getElementById('id_punto_control').value
-    };
-    try {
-        const response = await fetch('/api/Usuario/Actualizar', {
-            method: 'PUT',
-            headers: {
-                'Content.Type': 'application/json'
-            },
-            body: JSON.stringify(usuarioEditado)
-        });
-
-        if (response.ok) {
-            alert('Usuario actualizado exitosamente!');
-
-        } else {
-            alert('Error al intentar actualizar el usuario');
-        }
-
-    } catch (error) {
-        console.error('Error al guardar cambios del usuario:', error);
-    }
-}
-
-
 
 //Funcion para crear un nuevo usuario(admin)
 
@@ -218,4 +122,4 @@ async function crearNuevoUsuario() {
         console.error('Error al intentar crear el usuario', error);
     }
 }
-*/
+
