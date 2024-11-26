346bb7b05659ea2135daf616b0c1e73776eff056
Thiahho <thiagobrizuelav@gmail.com>
2024-11-25 23:16:47 -0300

    25/11/2024 Controller de log.

---
+++ SRVCAplicacion/Controllers/LogAudController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SRVCAplicacion.Data;
+using SRVCAplicacion.Models;
+
+namespace SRVCAplicacion.Controllers
+{
+    [ApiController]
+    [Route("api/[controler]")]
+    public class LogAudController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LogAudController(ApplicationDbContext app)
+        {
+            _context = app;
+        }
+
+
+        [HttpPost("Registrar")]
+        public async Task<IActionResult> RegistrarLogAud([FromBody] log_aud registroAuditoria)
+        {
+            try
+            {
+                // Valida que el registro no esté vacío
+                if (registroAuditoria == null)
+                {
+                    return BadRequest("Los datos de auditoría no pueden ser nulos.");
+                }
+
+                registroAuditoria.hora = DateTime.UtcNow;
+                await _context.log_Aud.AddAsync(registroAuditoria);
+                await _context.SaveChangesAsync();
+
+                return Ok("Registro de auditoría creado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error al registrar auditoría: {ex.Message}");
+            }
+        }
+    }
+}
