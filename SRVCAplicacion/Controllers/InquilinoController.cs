d152edc7549327f622940c367a075e09e32777bd
Thiahho <thiagobrizuelav@gmail.com>
2024-12-13 22:32:51 -0300

    13/12/2024 Controller Inquilino con funcion de actualizar inqui e visita

--- SRVCAplicacion/Controllers/InquilinoController.cs
+++ SRVCAplicacion/Controllers/InquilinoController.cs
@@ -137,59 +137,59 @@ namespace SRVCAplicacion.Controllers
             }
         }
 
-        //[HttpPost("CrearInquilino")]
-        //public async Task<IActionResult> PostInquilino([FromBody] visitante_inquilino inquilino)
-        //{
-        //    // Obtener el valor del claim "id_usuario".
-        //    var idUsuarioClaim = User.Claims.FirstOrDefault(c => c.Type == "id_usuario");
+        [HttpPost("CrearInquilino")]
+        public async Task<IActionResult> PostInquilino([FromBody] visitante_inquilino inquilino)
+        {
+            // Obtener el valor del claim "id_usuario".
+            var idUsuarioClaim = User.Claims.FirstOrDefault(c => c.Type == "id_usuario");
 
-        //    //Parsea el valor de la claim a int, las claim solo guardan string.
-        //    int idUsarioLog = int.Parse(idUsuarioClaim.Value);
+            //Parsea el valor de la claim a int, las claim solo guardan string.
+            int idUsarioLog = int.Parse(idUsuarioClaim.Value);
 
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-        //    try
-        //    {
-        //        await _context.visitante_Inquilino.AddAsync(inquilino);
-        //        await _context.SaveChangesAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _context.visitante_Inquilino.AddAsync(inquilino);
+                await _context.SaveChangesAsync();
 
-        //        var log = new log_aud
-        //        {
+                var log = new log_aud
+                {
 
-        //            //id_usuario = visitante.id_visitante_inquilino,
+                    //id_usuario = visitante.id_visitante_inquilino,
 
-        //            id_usuario = idUsarioLog,
-        //            accion = "Creación de inquilino",
-        //            valor_original = null,
-        //            //valor_nuevo = $"Usuario:{usuario.usuario}, Email:{usuario.email}, Dni:{usuario.dni}, {}",
-        //            valor_nuevo = $"Nombre:{inquilino.nombre}, Dni:{inquilino.identificacion}, Activo:{inquilino.activo}, Telefono:{inquilino.telefono}," +
-        //                         $"Estado:{inquilino.estado}, Punto Control:{inquilino.id_punto_control}, Tabla:'visitante_inquilino'",
-        //            hora = DateTime.UtcNow,
-        //            id_punto_control = inquilino.id_punto_control,
-        //            estado_actualizacion = 1
-        //        };
-
-
-        //        // Log para verificar los datos del log
-        //        Console.WriteLine($"Intentando registrar log: {log.valor_nuevo}");
-
-        //        await _auditoria.RegistrarCambio(log);
-        //        return CreatedAtAction(nameof(GetIdentificacion), new { id = inquilino.identificacion }, inquilino);
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(new { error = ex.Message });
-        //    }
-        //}
+                    id_usuario = idUsarioLog,
+                    accion = "Creación de inquilino",
+                    valor_original = null,
+                    //valor_nuevo = $"Usuario:{usuario.usuario}, Email:{usuario.email}, Dni:{usuario.dni}, {}",
+                    valor_nuevo = $"Nombre:{inquilino.nombre}, Dni:{inquilino.identificacion}, Activo:{inquilino.activo}, Telefono:{inquilino.telefono}," +
+                                 $"Estado:{inquilino.estado}, Punto Control:{inquilino.id_punto_control}, Tabla:'visitante_inquilino'",
+                    hora = DateTime.UtcNow,
+                    id_punto_control = inquilino.id_punto_control,
+                    estado_actualizacion = 1
+                };
 
 
+                // Log para verificar los datos del log
+                Console.WriteLine($"Intentando registrar log: {log.valor_nuevo}");
+
+                await _auditoria.RegistrarCambio(log);
+                return CreatedAtAction(nameof(GetIdentificacion), new { id = inquilino.identificacion }, inquilino);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
 
 
-        [HttpPut("identificacion")]
-        public async Task<IActionResult> Actualizar(int id, [FromBody] visitante_inquilino visitante_Inquilino)
+
+
+        [HttpPut("ActualizarInquilino")]
+        public async Task<IActionResult> ActualizarInquilino(int id, [FromBody] visitante_inquilino visitante_Inquilino)
         {
 
             if (visitante_Inquilino == null)
@@ -220,7 +220,7 @@ namespace SRVCAplicacion.Controllers
                 var logAud = new log_aud
                 {
                     id_usuario = id,
-                    accion = "Actualización de usuario",
+                    accion = "Actualización de inquilino",
                     hora = DateTime.Now,
                     valor_original = valorOriginal,
                     valor_nuevo = valorNuevo,
@@ -243,6 +243,60 @@ namespace SRVCAplicacion.Controllers
                 return BadRequest(new { mensaje = "Error al guardar los cambios", detalle = ex.Message });
             }
         }
+        [HttpPut("ActualizarVisita")]
+        public async Task<IActionResult> ActualizarVisita(int id, [FromBody] visitante_inquilino visitante_Inquilino)
+        {
 
+            if (visitante_Inquilino == null)
+            {
+                return BadRequest(new { mensaje = "Datos vacios." });
+            }
+
+            var inquilinoExistente = _context.visitante_Inquilino.FirstOrDefault(u => u.id_visitante_inquilino == id);
+
+            if (inquilinoExistente == null)
+            {
+                return NotFound(new { mensaje = $"Usuario con ID {id} no encontrado." });
+            }
+
+            var valorOriginal = $"Usuario:{inquilinoExistente.nombre}, Dni:{inquilinoExistente.identificacion},Estado:{inquilinoExistente.estado},Activo:{inquilinoExistente.activo}, Punto Control:{inquilinoExistente.id_punto_control}";
+            var valorNuevo = $"Usuario:{visitante_Inquilino.nombre}, Dni:{visitante_Inquilino.identificacion},Estado:{visitante_Inquilino.estado},Activo:{visitante_Inquilino.activo}, Punto Control:{visitante_Inquilino.id_punto_control}";
+
+            inquilinoExistente.nombre = visitante_Inquilino.nombre;
+            inquilinoExistente.identificacion = visitante_Inquilino.identificacion;
+            inquilinoExistente.telefono = visitante_Inquilino.telefono;
+            inquilinoExistente.estado = visitante_Inquilino.estado;
+            inquilinoExistente.activo = visitante_Inquilino.activo;
+            inquilinoExistente.id_punto_control = visitante_Inquilino.id_punto_control;
+
+            try
+            {
+
+                var logAud = new log_aud
+                {
+                    id_usuario = id,
+                    accion = "Actualización de Visita",
+                    hora = DateTime.Now,
+                    valor_original = valorOriginal,
+                    valor_nuevo = valorNuevo,
+                    //tabla = "Usuario",
+                    id_punto_control = visitante_Inquilino.id_punto_control
+                };
+                if (string.IsNullOrEmpty(logAud.valor_original) || string.IsNullOrEmpty(logAud.valor_nuevo))
+                {
+                    return NotFound(new { mensaje = "Error al generar el log de auditoría. Los valores originales o nuevos están vacíos." });
+                }
+
+                //_appDbContext.log_Aud.Add(logAud);
+                await _context.SaveChangesAsync();
+                await _auditoria.RegistrarCambio(logAud);
+
+                return Ok(new { mensaje = "Inquilino actualizado con éxito" });
+            }
+            catch (DbUpdateException ex)
+            {
+                return BadRequest(new { mensaje = "Error al guardar los cambios", detalle = ex.Message });
+            }
+        }
     }
 }
